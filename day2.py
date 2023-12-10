59440d71dd6a486b95a6bee4e0002d1fa226ad78
Vivien Würth <wurthvivien@gmail.com>
2023-12-10 13:31:24 +0100

    day 2 task 2 completed

--- day2.py
+++ day2.py
@@ -1,5 +1,6 @@
-""""#Task 1
+# Task 1
 import re
+
 file = open("day2.txt", "r")
 lines = file.readlines()
 file.close()
@@ -11,42 +12,72 @@ blue = 14
 for line in lines:
     game = ''.join(re.findall(r'\d\d?\d?:', line))
     game = game[:-1]
-    print("game ", game)
     good = 1
 
     cols = re.findall(r'\d+ ...?.?.', line)
-
-    print(cols)
 
     for col in cols:
         if 'red' in col:
             r = ''.join(re.findall(r'\d?\d', col))
             if int(r) > red or not good:
                 good = 0
-                print("Too much red", r)
                 break
 
         if 'green' in col or not good:
             g = ''.join(re.findall(r'\d?\d', col))
             if int(g) > green:
                 good = 0
-                print("Too much green", g)
                 break
 
         if 'blue' in col or not good:
             b = ''.join(re.findall(r'\d?\d', col))
             if int(b) > blue:
                 good = 0
-                print("Too much blue", b)
                 break
 
     if good:
         answer += int(game)
 
-    print("answer:", answer)
+print("final answer task 1", answer)
 
+# Task 2
 
+import re
 
-print("final answer", answer)
+file = open("day2.txt", "r")
+lines = file.readlines()
+file.close()
+answer = 0
+red = 0
+green = 0
+blue = 0
 
-"""
+for line in lines:
+    game = ''.join(re.findall(r'\d\d?\d?:', line))
+    game = game[:-1]
+
+    cols = re.findall(r'\d+ ...?.?.', line)
+
+    for col in cols:
+        if 'red' in col:
+            r = ''.join(re.findall(r'\d?\d', col))
+            if int(r) > red:
+                red = int(r)
+
+        if 'green' in col:
+            g = ''.join(re.findall(r'\d?\d', col))
+            if int(g) > green:
+                green = int(g)
+
+        if 'blue' in col:
+            b = ''.join(re.findall(r'\d?\d', col))
+            if int(b) > blue:
+                blue = int(b)
+
+    answer = answer + red * green * blue
+
+    red = 0
+    green = 0
+    blue = 0
+
+print("final answer task 2", answer)
